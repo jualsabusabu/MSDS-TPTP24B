f74942fe2c3f5f1dcad4f1e30889b1ced575e94b
jualsabusabu <muhammadpanji@mhs.cwe.ac.id>
2026-04-14 18:33:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const emptyState = document.getElementById('emptyState');
 function enterApp() {
   document.getElementById('landing').style.display = 'none';
   document.getElementById('app').style.display = 'flex';
+
   showSection('msds');
 }
 
@@ -23,12 +24,12 @@ function showSection(id, el) {
   if (el) el.classList.add('active');
 }
 
-// IG LINK
+// LOGO → IG
 function openIG() {
   window.open("https://instagram.com/USERNAME_LO", "_blank");
 }
 
-// LOAD
+// LOAD DATA
 fetch('./msds.json')
   .then(res => res.json())
   .then(json => {
@@ -45,10 +46,13 @@ search.addEventListener('input', function () {
 // RENDER
 function render(items) {
   list.innerHTML = '';
+
   items.forEach(item => {
     const li = document.createElement('li');
     li.textContent = item.title;
+
     li.onclick = () => openFile(item.file, item.title);
+
     list.appendChild(li);
   });
 }
@@ -56,7 +60,13 @@ function render(items) {
 // OPEN FILE
 function openFile(file, title) {
 
+  const isMobile = window.innerWidth <= 768;
   const url = window.location.origin + file;
+
+  if (isMobile) {
+    window.open(url, "_blank");
+    return;
+  }
 
   viewerFrame.src = url + "#zoom=page-width";
   viewerTitle.textContent = title;
@@ -65,7 +75,7 @@ function openFile(file, title) {
   viewerFrame.style.display = "block";
 }
 
-// ===== GALLERY =====
+/* ===== GALLERY ===== */
 const images = document.querySelectorAll('#galleryGrid img');
 const modal = document.getElementById('modal');
 const modalImg = document.getElementById('modalImg');
