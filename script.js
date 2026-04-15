961eb407d92974e58ed2bd7667466149b9470134
user0129383341 <muhammadpanji@mhs.cwe.ac.id>
2026-04-15 20:14:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,77 +8,61 @@ function enterApp() {
   document.getElementById('app').style.display = 'flex';
 
   const audio = document.getElementById("bgm");
-  audio.volume = 0.4;
-  audio.play().catch(() => {});
+  audio.volume = 0.3;
+  audio.play().catch(()=>{});
 }
 
 function showSection(id, el) {
-  document.querySelectorAll('.section').forEach(s => s.classList.remove('active'));
+  document.querySelectorAll('section').forEach(s => s.classList.remove('active'));
   document.getElementById(id).classList.add('active');
 
   document.querySelectorAll('.sidebar button').forEach(b => b.classList.remove('active'));
-  if (el) el.classList.add('active');
+  el.classList.add('active');
 }
 
 fetch('./msds.json')
-  .then(res => res.json())
-  .then(json => {
-    data = json;
-    render(data);
-  });
+.then(res => res.json())
+.then(json => {
+  data = json;
+  render(data);
+});
 
-function render(items) {
+function render(items){
   list.innerHTML = '';
 
-  items.forEach(item => {
+  items.forEach(item=>{
     const li = document.createElement('li');
     li.textContent = item.title;
 
-    li.onclick = () => {
+    li.onclick = ()=>{
       document.getElementById('viewerTitle').innerText = item.title;
-      window.open(window.location.origin + item.file, "_blank");
+      window.open(window.location.origin + item.file);
     };
 
     list.appendChild(li);
   });
 }
 
-search.addEventListener('input', function () {
+search.addEventListener('input', function(){
   const keyword = this.value.toLowerCase();
-  const filtered = data.filter(item =>
-    item.title.toLowerCase().includes(keyword)
-  );
-  render(filtered);
+  render(data.filter(i=>i.title.toLowerCase().includes(keyword)));
 });
 
-function toggleMusic() {
+function toggleMusic(){
   const audio = document.getElementById("bgm");
   audio.paused ? audio.play() : audio.pause();
 }
 
-/* GALLERY */
-const images = document.querySelectorAll('#galleryGrid img');
+/* gallery */
+const imgs = document.querySelectorAll('#galleryGrid img');
 const modal = document.getElementById('modal');
 const modalImg = document.getElementById('modalImg');
 
-let index = 0;
-
-images.forEach((img, i) => {
-  img.onclick = () => {
+imgs.forEach(img=>{
+  img.onclick = ()=>{
     modal.style.display = 'flex';
     modalImg.src = img.src;
-    index = i;
-  };
+  }
 });
 
-document.getElementById('closeBtn').onclick = () => modal.style.display = 'none';
-
-document.querySelector('.next').onclick = () => {
-  index = (index + 1) % images.length;
-  modalImg.src = images[index].src;
-};
-
-document.querySelector('.prev').onclick = () => {
-  index = (index - 1 + images.length) % images.length;
-  modalImg.src = images[index].src;
-};
+modal.onclick = ()=> modal.style.display = 'none';
